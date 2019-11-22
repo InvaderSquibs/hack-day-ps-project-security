7325208a682fa163d7027d790e703e31b321cb3a
Zach Walton <zachary-walton@pluralsight.com>
2019-11-22 09:31:20 -0700

    Redirect errors from runner

--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
-docker stop hd > /dev/null
-docker rm hd > /dev/null
+docker stop hd > /dev/null 2>/dev/null
+docker rm hd > /dev/null 2>/dev/null
 docker run -d -p 1337:22 --name hd squibs/hack-day
-ssh-keygen -R [localhost]:1337 > /dev/null
+ssh-keygen -R [localhost]:1337 > /dev/null 2>&1
 echo "image squibs/hack-day running"
 echo "password is 'HACKDAY'"
 echo "use port 1337 to ssh into the docker container"
